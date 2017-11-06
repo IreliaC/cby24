87b4f80bb9f44095f9d88017be5206324eff46dc
chenby <chenby4253@joyowo.com>
2017-11-06 12:23:13 +0800

    add a dog of pojo at company

---
+++ src/main/java/com/joyowo/mytest/pojo/Dog.java
@@ -0,0 +1,34 @@
+package com.joyowo.mytest.pojo;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * User: chenby
+ * Date: 2017-11-06
+ * ----------Dragon be here!----------/
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑
+ * 　　　　┃　　　┃代码无BUG！
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━神兽出没━━━━━━
+ */
+@Data
+@ToString
+public class Dog {
+    private String dogName;
+    
+}
