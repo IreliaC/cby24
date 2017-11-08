8ea84847f9e765495bd2a0cbf06a92559227624a
chenby <chenby4253@joyowo.com>
2017-11-08 17:48:47 +0800

    提交

---
+++ src/main/java/com/joyowo/mytest/controller/AopController.java
@@ -0,0 +1,25 @@
+package com.joyowo.mytest.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * User: chenby
+ * Date: 2017-11-08
+ */
+@RestController
+@RequestMapping("/aop")
+public class AopController {
+
+    @RequestMapping("/before")
+    public String doBefore(Integer number) {
+
+        return "200";
+    }
+
+    @RequestMapping("/afterReturning")
+    public String doAfterReturning(String keys) {
+        return "200";
+    }
+}
