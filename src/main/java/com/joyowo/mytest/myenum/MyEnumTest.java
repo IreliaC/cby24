4a2c6347498eb8a3d7e50253fecab34373edebc3
IreliaC <290880219@qq.com>
2017-11-19 13:55:10 +0800

    add at home

---
+++ src/main/java/com/joyowo/mytest/myenum/MyEnumTest.java
@@ -0,0 +1,12 @@
+package com.joyowo.mytest.myenum;//
+
+
+/**
+ * Created by ireliac on 2017/11/15.
+ */
+public class MyEnumTest {
+    public static void main(String[] args) {
+        MyEnum e = MyEnum.FLAMING;
+        System.out.println(e);
+    }
+}
