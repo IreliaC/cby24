4502f748b671ab8838bbce7d3861620509dd4c7a
IreliaC <290880219@qq.com>
2017-12-06 22:34:16 +0800

    aaa

--- eurekaclient/src/main/java/com/joyowo/eurekaclient/EurekaclientApplication.java
+++ eurekaclient/src/main/java/com/joyowo/eurekaclient/EurekaclientApplication.java
@@ -1,8 +1,5 @@
 package com.joyowo.eurekaclient;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
